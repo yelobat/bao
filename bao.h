865efbb1a59662686ae8533e5f3a08b206af5823
wwotz <wwotz12@gmail.com>
2024-08-26 21:45:16 +0100

    updated bao.h implemented list ADT.

--- bao.h
+++ bao.h
@@ -36,12 +36,26 @@ struct bao_array_t {
 
 typedef struct bao_array_t *bao_array_t;
 
+struct bao_list_t {
+	struct bao_list_t *rest;
+	void *data;
+};
+
+typedef struct bao_list_t *bao_list_t;
+
 BAOLIBDEF bao_array_t bao_array_create(size_t size, size_t memb_size);
 BAOLIBDEF int         bao_array_insert(bao_array_t array, void *v);
 BAOLIBDEF void *      bao_array_get(bao_array_t array, size_t i);
 BAOLIBDEF void *      bao_array_find(bao_array_t array, void *v,
 				     int (*compare)(void *, void *));
 BAOLIBDEF void        bao_array_free(bao_array_t *array);
+
+BAOLIBDEF bao_list_t  bao_list_create(void *v);
+BAOLIBDEF bao_list_t  bao_list_push(bao_list_t list, void *v);
+BAOLIBDEF bao_list_t  bao_list_pop(bao_list_t list, void **v);
+BAOLIBDEF void *      bao_list_get(bao_list_t list, size_t i);
+BAOLIBDEF bao_list_t  bao_list_append(bao_list_t a_list, bao_list_t b_list);
+BAOLIBDEF void        bao_list_free(bao_list_t *list);
 
 #ifdef BAO_IMPLEMENTATION
 
@@ -144,6 +158,81 @@ BAOLIBDEF void bao_array_free(bao_array_t *array)
 	assert(array && *array);
 	BAO_FREE((*array)->data);
 	BAO_FREE(*array);
+}
+
+BAOLIBDEF bao_list_t bao_list_create(void *v)
+{
+	bao_list_t list;
+
+	assert(v);
+	
+	list = BAO_MALLOC(sizeof(*list));
+	if (!list) {
+		return NULL;
+	}
+
+	list->data = v;
+	list->rest = NULL;
+	return list;
+}
+
+BAOLIBDEF bao_list_t bao_list_push(bao_list_t list, void *v)
+{
+	bao_list_t rest;
+	assert(list);
+	assert(v);
+
+	rest = bao_list_create(v);
+	if (!rest) {
+		return list;
+	}
+
+	rest->data = v;
+	rest->rest = list;
+	return rest;
+}
+
+BAOLIBDEF bao_list_t bao_list_pop(bao_list_t list, void **v)
+{
+	bao_list_t head;
+	if (!list) return list;
+
+	head = list->rest;
+	if (v) *v = list->data;
+	BAO_FREE(list);
+	return head;
+}
+
+BAOLIBDEF void *bao_list_get(bao_list_t list, size_t i)
+{
+	size_t j;
+	for (j = 0; j < i && list; j++) {
+		list = list->rest;
+	}
+
+	return list ? list->data : NULL;
+}
+
+BAOLIBDEF bao_list_t bao_list_append(bao_list_t a_list, bao_list_t b_list)
+{
+	bao_list_t *head = &a_list;
+
+	while (*head)
+		head = &(*head)->rest;
+	*head = b_list;
+	return a_list;
+}
+
+BAOLIBDEF void bao_list_free(bao_list_t *list)
+{
+	bao_list_t next;
+
+	assert(list);
+	for (; *list; *list = next) {
+		next = (*list)->rest;
+		BAO_FREE((*list)->data);
+		BAO_FREE(*list);
+	}
 }
 
 #endif /* BAO_IMPLEMENTATION */
